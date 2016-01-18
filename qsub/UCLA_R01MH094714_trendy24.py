9745a8ccf55c220b742cb8c0b264f49e17d020e2
Dominic Fitzgerald <dominicfitzgerald11@gmail.com>
2016-01-18 11:36:31 -0600

    Fixed 'order.log', again

--- qsub/UCLA_R01MH094714_trendy24.py
+++ qsub/UCLA_R01MH094714_trendy24.py
@@ -57,8 +57,8 @@ for i, ucla_pair in enumerate(ucla_files):
 
     # Change to logs directory so qsub output will go here
     os.chdir(os.path.join(ucla_pair_out, 'logs'))
-    subprocess.call(['echo', '"{} of {}"'.format(i + 1, len(ucla_files)),
-                     '>', 'order.log'])
+    subprocess.call(['echo', '"{} of {}"'.format(i + 1, len(ucla_files))],
+                    stdout=open('order.log', 'w'))
 
     subprocess.call(['qsub', '-v', ','.join(ucla_args), ucla_pbs])
     print 'Submitted job {} of {}'.format(i + 1, len(ucla_files))
